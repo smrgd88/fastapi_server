46267b76f2c7fb102caab68abcc6303923cbf31a
hskeum <hskeum@surromind.ai>
2022-09-16 04:27:53 +0900

    github push test

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
 async def index():
     return {'message': 'Hello World!'}
+
+
+@app.get('/test')
+async def test():
+    return {'message': 'This is Test'}
